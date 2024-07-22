330a5f44bbfcb6e574ec74641e059a0813d59d09
Aryan Kumar <135582980+karyansingh@users.noreply.github.com>
2024-07-22 21:59:52 +0530

    Time: 7 ms (90.64%), Space: 45.5 MB (10.15%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.java
+++ 2418-sort-the-people/2418-sort-the-people.java
@@ -1,14 +1,17 @@
 class Solution {
     public String[] sortPeople(String[] names, int[] heights) {
-        TreeMap<Integer,String>map=new TreeMap<>();
-        for(int i=0;i<names.length;i++){
+        TreeMap<Integer,String> map=new TreeMap<>();
+
+        for(int i=0;i<names.length;i++)
+        {
             map.put(heights[i],names[i]);
         }
-        int j=names.length-1;
-      for (Map.Entry<Integer, String> entry : map.entrySet()) {
-           names[j]=entry.getValue();
-           j--;
-       }
-       return names;
+        String[] res=new String[names.length];
+        int i=names.length-1;
+        for(Map.Entry ele:map.entrySet())
+        {
+            res[i--]=ele.getValue().toString();
+        }
+        return res;
     }
 }
