54c71d287f2fbb4660746bd3027ecae4f241f670
Aryan Kumar <135582980+karyansingh@users.noreply.github.com>
2024-08-01 22:34:14 +0530

    Time: 1 ms (82.64%), Space: 42.6 MB (45.62%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
@@ -3,8 +3,8 @@ class Solution {
         int count=0;
         for(int i=0;i<details.length;i++)
         {
-          
-            if(Integer.parseInt(details[i].substring(11,13))>60)
+            String str=details[i].substring(11,13);
+            if(Integer.parseInt(str)>60)
             count++;
         }
         return count;
