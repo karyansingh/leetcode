f352a434a6ebd22f6ae792a0a63f9916a82896f0
Aryan Kumar <135582980+karyansingh@users.noreply.github.com>
2024-09-26 11:14:49 +0530

    Time: 10 ms (9.43%), Space: 41 MB (84.43%) - LeetHub

---
+++ 2475-number-of-unequal-triplets-in-array/2475-number-of-unequal-triplets-in-array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int unequalTriplets(int[] nums) {
+        int count = 0;
+        int i = 0, j = 1, k = 2;
+        while(i<nums.length-2)
+        {
+            if(nums[i] != nums[j] && nums[j] != nums[k] && nums[i] != nums[k]){
+                count++;
+            }
+            k++;
+            if(k == nums.length){
+                j++;
+                k = j+1;
+            }
+            if(j == nums.length - 1){
+                i++;
+                j = i + 1;
+                k = j + 1;
+            }
+        }
+        return count;
+    }
+}
