5da48df34da72521cfc6e9bf6462879f22f26079
Aryan Kumar <135582980+karyansingh@users.noreply.github.com>
2024-10-18 10:52:00 +0530

    Time: 1 ms (55.42%), Space: 42.6 MB (51.2%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,26 +1,24 @@
 class Solution {
-    public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>>resultList=new ArrayList<>();
-        //backtracking from the begning
-        backtrack(resultList,new ArrayList<>(),nums,0);
-        return resultList;
+    List<List<Integer>> result;
 
-    }
-    private void backtrack(List<List<Integer>>resultSets,List<Integer>tempSet,
-    int []nums,int start)
-    {
-        //Add result to result set
-        resultSets.add(new ArrayList<>(tempSet));
-        for(int i=start;i<nums.length;i++)
-        {
-            //including the number
-            tempSet.add(nums[i]);
-
-            //backtrack the new subset
-            backtrack(resultSets,tempSet,nums,i+1);
-
-            //not including
-            tempSet.remove(tempSet.size()-1);
+    void solve(int index, int nums[], List<Integer> temp) {
+        // Base Case
+        if (index >= nums.length) {
+            result.add(new ArrayList<>(temp)); // Add a deep copy of temp
+            return;
         }
+        // Include the current element
+        temp.add(nums[index]);
+        solve(index + 1, nums, temp);
+        // Exclude the current element
+        temp.remove(temp.size() - 1);
+        solve(index + 1, nums, temp);
+    }
+
+    public List<List<Integer>> subsets(int[] nums) {
+        result = new ArrayList<>(); // Initialize result here
+        List<Integer> temp = new ArrayList<>();
+        solve(0, nums, temp);
+        return result;
     }
 }
