e999948760cf93fe711dbe16d91e642ee8a16168
Aryan Kumar <135582980+karyansingh@users.noreply.github.com>
2026-02-09 15:13:27 +0530

    Time: 936 ms (53.2%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,8 +1,24 @@
 /* Write your T-SQL query statement below */
+/*
 SELECT Department, Employee, Salary
 FROM(
     SELECT D.name AS Department, E.name AS Employee, E.salary AS Salary,
     DENSE_RANK() OVER (PARTITION BY departmentId ORDER BY salary DESC) AS d_rank
     FROM Employee E INNER JOIN Department D ON E.departmentId = D.id
 ) T
+WHERE d_rank <= 3;
+*/
+
+WITH dept_salary AS (
+    SELECT 
+        d.name AS Department,
+        e.name AS Employee,
+        e.salary AS Salary,
+        DENSE_RANK() OVER (PARTITION BY e.departmentId ORDER BY e.salary DESC) AS d_rank
+    FROM Employee e
+    INNER JOIN Department d 
+        ON e.departmentId = d.id
+)
+SELECT Department,Employee,Salary
+FROM dept_salary
 WHERE d_rank <= 3;
